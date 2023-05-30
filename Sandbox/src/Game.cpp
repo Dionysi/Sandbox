ebf517fc649876a5340ac105d55920ee64e7fe6e
Dionysi <dionysialex@gmail.com>
2023-05-30 15:40:16 +0200

    documentation

--- Sandbox/src/Game.cpp
+++ Sandbox/src/Game.cpp
@@ -3,18 +3,43 @@
 
 Game::Game()
 {
+	// Call your initializers from the constructor.
+}
 
+Game::~Game()
+{
+	// Perform any saving operations and free allocated memory.
 }
 
 void Game::Tick(float dt)
 {
+	// Update average frametime.
+	m_AvgFrameTime = 0.99f * m_AvgFrameTime + 0.01 * dt;
+
 	// Game logic goes here.
+
 }
 
 void Game::Draw(float dt)
 {
 	// Rendering goes here.
 
+	for (int y = 0; y < Application::RenderHeight(); y++)
+	{
+		for (int x = 0; x < Application::RenderWidth(); x++)
+		{
+			Color color =
+			{
+				0,
+				(uchar)(255.0f * ((float)x / (float)Application::RenderWidth())),
+				(uchar)(255 - (uchar)(255.0f * (float)y / (float)Application::RenderHeight())),
+				(uchar)255
+			};
+			Application::Screen()->PlotPixel(color, x, y);
+		}
+	}
+
+	Application::Screen()->SyncPixels();
 }
 
 void Game::RenderGUI(float dt)
--- Sandbox/src/Game.h
+++ Sandbox/src/Game.h
@@ -1,13 +1,47 @@
 #pragma once
 #include "Template/Application.h"
 
+/*
+* Implement your game logic in this class. The order of function calls is:
+* 1) Tick
+* 2) Draw
+* 3) RenderGUI
+*/
 class Game
 {
 
+private:
+
+	/*
+	* Average time it takes to process a frame.
+	* For debugging purpose only.
+	*/
+	float m_AvgFrameTime = 0.0f;
+
 public:
+	/*
+	* Call your initializers from the constructor.
+	*/
 	Game();
+	/*
+	* Perform any saving operations and free allocated memory.
+	*/
+	~Game();
+
+	/*
+	* Use the Tick function to implement your game logic.
+	* @param[in] dt				Time since previous Tick call in seconds.
+	*/
 	void Tick(float dt);
+	/*
+	* Use the draw function to implement any non-gui related rendering.
+	* @param[in] dt				Time since previous call in seconds.
+	*/
 	void Draw(float dt);
+	/*
+	* Use the render gui function to implement any gui related rendering using ImGui.
+	* @param[in] dt				Time since previous call in seconds.
+	*/
 	void RenderGUI(float dt);
 };
 
